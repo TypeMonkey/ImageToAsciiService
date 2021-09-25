f1e59679fa64fc8e12b05e3af1ba414f30660bc5
Jose <gaurojose@gmail.com>
2021-09-25 02:33:59 -0700

    ping route added

--- main.js
+++ main.js
@@ -18,3 +18,24 @@ imageToAscii("https://octodex.github.com/images/privateinvestocat.jpg", {
     console.log(stringArray)
 });
 */
+
+const express = require("express");
+const app = express();
+
+const PORT = 80;
+
+//app.post();
+
+//Sets the route for "ping"
+app.get("/ping", (req, res, next) => {
+    res.status(200).json({success:true});
+});
+
+
+//Starts the server on the provided port
+const server = app.listen(PORT, () => {
+    console.log("Running server on port: "+PORT);
+});
+
+//We export "server" for testing purposes
+module.exports = server;
