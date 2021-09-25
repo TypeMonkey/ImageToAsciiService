1549f1e52b632fd354099e054ae153a749f102e5
Jose <gaurojose@gmail.com>
2021-09-25 02:24:16 -0700

    init commit

---
+++ main.js
@@ -0,0 +1,20 @@
+console.log("Hello world");
+/*
+// Dependencies
+const imageToAscii = require("image-to-ascii");
+ 
+// Passing options
+imageToAscii("https://octodex.github.com/images/privateinvestocat.jpg", {
+    colored: false,
+    concat: false,
+    size: {
+        height: 80,
+        width:  80
+    }
+}, (err, converted) => {
+    const stringArray = converted.map(x => x.join(""));
+    console.log(typeof converted);
+    console.log(stringArray.length);
+    console.log(stringArray)
+});
+*/
